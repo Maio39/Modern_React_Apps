5e8d6dec40843db1cab07e022ce0672e73f62bd1
Marco Maier <marcomaier3903@gmail.com>
2023-04-03 21:44:58 +0200

    Add style and animation in App.css

--- gpt3_marco/src/index.js
+++ gpt3_marco/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import './index.css';
 
 /* Con questo comando stiamo dicendo che tutta l'app sarà all interno del tag div con id root */
 ReactDOM.render(<App />, document.getElementById('root')); 
