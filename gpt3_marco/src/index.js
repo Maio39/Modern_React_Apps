2fcd1542ad21d9c92464fe3a90bf3be4dff41b26
Marco Maier <marcomaier3903@gmail.com>
2023-04-03 19:34:41 +0200

    Add Components

--- gpt3_marco/src/index.js
+++ gpt3_marco/src/index.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import App from './App';
+
+/* Con questo comando stiamo dicendo che tutta l'app sarà all interno del tag div con id root */
+ReactDOM.render(<App />, document.getElementById('root')); 
